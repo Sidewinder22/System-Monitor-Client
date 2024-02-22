72fbf66d443c4a5bf3ec9b9593f1a7b85dfbd392
Sidewinder22 <sidewinder22@protonmail.com>
2024-02-22 19:51:27 +0100

    Add read_file

--- system-monitor-client/src/main.rs
+++ system-monitor-client/src/main.rs
@@ -1,26 +1,32 @@
-use std::net::TcpListener;
+// use std::net::TcpListener;
 
-use system_monitor_client::handle_connection;
+// use system_monitor_client::handle_connection;
+
+mod fs;
 
 fn main() -> std::io::Result<()> {
     println!("### System Monitor Client ###");
 
-    const IP : &str = "127.0.0.1";
-    const PORT : u32 = 9999;
+    // fs::read_file();
+    // file_operations::read_file();
+    fs::file_operations::read_file();
+
+    // const IP : &str = "127.0.0.1";
+    // const PORT : u32 = 9999;
     
-    let connection_settings = format!("{}:{}", IP, PORT.to_string());
+    // let connection_settings = format!("{}:{}", IP, PORT.to_string());
 
-    let listener = TcpListener::bind(connection_settings)?;
+    // let listener = TcpListener::bind(connection_settings)?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_connection(stream).expect("Error in handle_connection");
-            }
-            Err(e) => {
-                println!("Connection failed: {}", e);
-            }
-        }
-    }
+    // for stream in listener.incoming() {
+    //     match stream {
+    //         Ok(stream) => {
+    //             handle_connection(stream).expect("Error in handle_connection");
+    //         }
+    //         Err(e) => {
+    //             println!("Connection failed: {}", e);
+    //         }
+    //     }
+    // }
     Ok(())
 }
