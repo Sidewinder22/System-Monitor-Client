6b6dd2927f68e13c434877315c7b0c44118dc26d
Sidewinder22 <sidewinder22@protonmail.com>
2024-02-23 19:58:23 +0100

    Introduce process info

--- system-monitor-client/src/fs/file_operations.rs
+++ system-monitor-client/src/fs/file_operations.rs
@@ -1,10 +1,30 @@
 use std::fs;
+use crate::fs::process_info::{ProcessInfo, ProcessState};
 
 pub fn read_file() {
 
     let path = "/proc/1681/status";
 
     let content = fs::read_to_string(path).expect("Can't read file!");
+    let lines = content.split('\n');
 
-    println!("Text:\n{content}");
+    let mut process_info = ProcessInfo {
+        pid: 0,
+        name: String::from("Name"),
+        state: ProcessState::Unknown,
+        threads: 0,
+    };
+
+    println!("Before reading, name: {}", process_info.name);
+
+    for line in lines {
+        println!("line: {line} ");
+
+        if line.contains("Name") {
+            process_info.name = line.to_string();
+        }
+    }
+
+    println!("After reading, name: {}", process_info.name);
+
 }
